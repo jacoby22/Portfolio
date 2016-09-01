7f818b6671893eae49a041c2315bdefd2e265310
Nick Jacoby <nickjacoby22@gmail.com>
2016-08-31 17:36:31 -0700

    rerouted all controller function calls using the context object and next

--- contactController.js
+++ contactController.js
@@ -1,9 +1,8 @@
 (function(module) {
   var contactController = {};
 
-  contactController.index = function() {
-    portfolio.selectNav();
-    console.log('contact');
+  contactController.index = function(ctx, next) {
+    next();
   };
 
   module.contactController = contactController;
